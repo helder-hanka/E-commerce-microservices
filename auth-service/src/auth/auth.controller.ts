92aee1ff77087fd380695ce3e9008a0505baf9e6
Helder <hfernandes238@gmail.com>
2025-08-05 13:03:03 +0200

    delete imp, delete and move updateProfile, updateRoles func

--- auth-service/src/auth/auth.controller.ts
+++ auth-service/src/auth/auth.controller.ts
@@ -4,18 +4,11 @@ import {
   Body,
   Get,
   Query,
-  UseGuards,
-  Req,
-  Param,
   ValidationPipe,
   BadRequestException,
   UsePipes,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthGuard } from '@nestjs/passport';
-import { RolesGuard } from './guards/roles.guard';
-import { Roles } from './decorators/roles.decorator';
-import { UpdateRolesDto } from 'src/user/dto/update-roles.dto';
 import { CreateUserDto } from 'src/user/dto/createUser.dto';
 
 @Controller('auth')
@@ -40,27 +33,4 @@ export class AuthController {
   async login(@Body() loginDto: any) {
     return this.authService.login(loginDto);
   }
-
-  //   @UseGuards(AuthGuard('jwt')) // Protège l'accès, nécessite un JWT valide
-  //   @Get('profile')
-  //   getProfile(@Req() req) {
-  //     return this.authService.getProfile(req.user.userId);
-  //   }
-
-  //   @UseGuards(AuthGuard('jwt'))
-  //   @Post('profile')
-  //   updateProfile(@Req() req, @Body() updateProfileDto: any) {
-  //     return this.authService.updateProfile(req.user.userId, updateProfileDto);
-  //   }
-
-  //   // Exemple d'un endpoint pour admin_application
-  //   @UseGuards(AuthGuard('jwt'), RolesGuard) // D'abord auth, puis rôles
-  //   @Roles('admin_application')
-  //   @Post('users/:id/roles')
-  //   async updateRoles(
-  //     @Param('id') userId: string,
-  //     @Body(new ValidationPipe()) updateRolesDto: UpdateRolesDto,
-  //   ) {
-  //     return this.authService.updateRoles(userId, updateRolesDto.roles);
-  //   }
 }
