f61c69051b64b6a1dd3b229c21a8825bb3bc710d
Helder <hfernandes238@gmail.com>
2025-08-05 13:07:20 +0200

    Create: UserJwtPayload, RequestWithUser and add in getProfile, createUpdateProfile func

--- auth-service/src/user/user.controller.ts
+++ auth-service/src/user/user.controller.ts
@@ -2,11 +2,11 @@ import {
   Controller,
   Post,
   Get,
-  Query,
   UseGuards,
   Req,
   Body,
   ValidationPipe,
+  UsePipes,
 } from '@nestjs/common';
 import { AuthService } from 'src/auth/auth.service';
 import { AuthGuard } from '@nestjs/passport';
@@ -14,21 +14,30 @@ import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { Param } from '@nestjs/common';
 import { UpdateRolesDto } from './dto/update-roles.dto';
+import { CreateProfileDto } from './dto/createProfile.dto';
+import type { RequestWithUser } from 'src/common/interfaces/request-with-user.interface.';
 
 @Controller('profile')
 export class UserController {
   constructor(private readonly authService: AuthService) {}
 
   @UseGuards(AuthGuard('jwt')) // Protège l'accès, nécessite un JWT valide
-  @Get('profile')
-  getProfile(@Req() req) {
+  @Get()
+  async getProfile(@Req() req: RequestWithUser) {
     return this.authService.getProfile(req.user.userId);
   }
 
   @UseGuards(AuthGuard('jwt'))
   @Post()
-  updateProfile(@Req() req, @Body() updateProfileDto: any) {
-    return this.authService.updateProfile(req.user.userId, updateProfileDto);
+  @UsePipes(new ValidationPipe({ transform: true }))
+  async createUpdateProfile(
+    @Req() req: RequestWithUser,
+    @Body() createProfileDto: CreateProfileDto,
+  ) {
+    return this.authService.createUpdateProfile(
+      req.user.userId,
+      createProfileDto,
+    );
   }
 
   // Exemple d'un endpoint pour admin_application
